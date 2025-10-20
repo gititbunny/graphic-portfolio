eed9f0d40b0f1e95ef035f636cad21fbcb214275
gititbunny <ninankhwashu@gmail.com>
2025-10-20 15:56:33 +0200

    Update header.jsx

---
+++ graphic-portfolio/src/components/Header.jsx
@@ -0,0 +1,22 @@
+import { Link, NavLink } from "react-router-dom";
+import "../styles/header.css";
+
+export default function Header() {
+  return (
+    <header className="site-header">
+      <div className="container header-inner">
+        <Link to="/" className="logo">
+          Nina Nkhwashu
+        </Link>
+        <nav className="nav">
+          <NavLink to="/" end>
+            Home
+          </NavLink>
+          <NavLink to="/work">Work</NavLink>
+          <NavLink to="/about">About</NavLink>
+          <NavLink to="/contact">Contact</NavLink>
+        </nav>
+      </div>
+    </header>
+  );
+}
