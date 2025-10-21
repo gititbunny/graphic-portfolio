cf9c26a93541be929eaa907ccf1e1b0940be0b14
gititbunny <ninankhwashu@gmail.com>
2025-10-21 06:46:00 +0200

    Update main.jsx

--- graphic-portfolio/src/main.jsx
+++ graphic-portfolio/src/main.jsx
@@ -1,14 +1,13 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
-import { BrowserRouter } from "react-router-dom"; // ← add
+import { BrowserRouter } from "react-router-dom";
 import App from "./App.jsx";
-import "./App.css";
+import "./styles/variables.css";
+import "./styles/globals.css";
 
 createRoot(document.getElementById("root")).render(
   <React.StrictMode>
     <BrowserRouter>
-      {" "}
-      {/* ← wrap App */}
       <App />
     </BrowserRouter>
   </React.StrictMode>
