4d8b5530d565014e3546c1c0a094408e87851dca
gititbunny <ninankhwashu@gmail.com>
2025-10-21 07:14:22 +0200

    Update routing structure

--- graphic-portfolio/src/App.jsx
+++ graphic-portfolio/src/App.jsx
@@ -1,10 +1,24 @@
+import { Routes, Route } from "react-router-dom";
 import Header from "./components/Header";
+import Footer from "./components/Footer";
+import Home from "./pages/Home";
+import Work from "./pages/Work";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import CaseStudy from "./pages/CaseStudy";
 
 export default function App() {
   return (
     <>
       <Header />
-      <main style={{ padding: "40px", fontSize: "20px" }}>Home</main>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/work" element={<Work />} />
+        <Route path="/work/:slug" element={<CaseStudy />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
